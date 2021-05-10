f44ea744643c2417ee0fc72125efdfd7a399c5f1
石渡拓海 <t-ishiwata@sight-visit.com>
2021-05-10 22:32:35 +0900

    assetsにmanifestファイルのpathを追加

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,11 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+webpack_manifest_path = Rails.root.join('public', 'assets', 'manifest.json')
+Rails.application.config.assets.webpack_manifest =
+ if File.exist?(webpack_manifest_path)
+   JSON.parse(File.read(webpack_manifest_path))
+ end
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
