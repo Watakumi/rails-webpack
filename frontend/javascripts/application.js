06141d51f735c2c74bbd6effeb068c2a1ee8839c
石渡拓海 <t-ishiwata@sight-visit.com>
2021-05-11 02:53:00 +0900

    tailwindcssを使う

--- frontend/javascripts/application.js
+++ frontend/javascripts/application.js
@@ -1,6 +1,6 @@
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-
+import "./styles.css"
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
