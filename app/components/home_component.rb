7eb40c9131a2654cd47b11e3bd4e91dc956549d4
石渡拓海 <t-ishiwata@sight-visit.com>
2021-05-12 01:48:26 +0900

    Use styles in erb tailwindcssをcomponent上に集約させて安全なUIを作り出す

--- app/components/home_component.rb
+++ app/components/home_component.rb
@@ -12,4 +12,14 @@ class HomeComponent < ApplicationComponent
   def sample_item
     OpenStruct.new(name: 'sample', description: 'This is sample component.')
   end
+
+  def style_element
+    {
+      container: 'p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4',
+      image_box: 'flex-shrink-0',
+      image_size: 'h-12 w-12',
+      item_name: 'text-xl font-medium text-black',
+      item_description: 'text-gray-500'
+    }
+  end
 end
